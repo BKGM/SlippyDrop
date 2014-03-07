4cefd74db906586b24ab1e0c9bc3a9626d73ade8
Nguyễn Tuấn Anh <anhnt.fami@gmail.com>
2014-03-08 03:48:26 +0700

    a

---
+++ js/blocks.js
@@ -0,0 +1,83 @@
+(function(){
+    var blockHeight   = 50 * SCALE,
+        blockGap      = 120 * SCALE,
+        maxLeftWidth  = WIDTH - blockGap,
+        blockDistance = 210 * SCALE,
+        maxY          = HEIGHT + blockHeight / 2;
+
+    Codea.Blocks = function(game){
+        this.blocks  = [];
+        this.current = 0;
+        this.height  = blockHeight;
+        this.game    = game;
+    };
+
+    Codea.Blocks.prototype = {
+        get: function(i){
+            return this.blocks[i];
+        },
+
+        head: function(){
+            return this.blocks[0];
+        },
+
+        last: function(){
+            var blocks = this.blocks;
+            return blocks[blocks.length-1];
+        },
+
+        now: function(){
+            return this.blocks[this.current];
+        },
+
+        spawn: function(pos_y){
+            pos_y = pos_y || 0;
+            var sy = pos_y - blockHeight;
+            var sw = this.game.random(blockGap, maxLeftWidth);
+            var swr = sw + blockGap
+            return this.blocks.push({y : sy, w : sw, wr : swr});
+        },
+
+        unshift: function(){
+            this.blocks.shift();
+            this.current--;
+        },
+
+        update: function(){
+
+            for (v in this.blocks) {
+                this.blocks[v].y += this.game.speed;
+            }
+
+            if (this.get(0).y >= maxY) this.unshift();
+
+            var s = this.last().y - blockDistance;
+            if (s >= 0) this.spawn(s);
+
+        },
+
+        pass: function(drop){
+            var condition = this.now().y > drop.top;
+            if (condition) this.current++;
+            return condition
+        },
+
+        draw: function(){
+            var blocks = this.blocks;
+            var game = this.game;
+            for (var i = 0, l = blocks.length; i < l; i++) {
+                var v = blocks[i];
+                game.fill(255, 255, 255, 255);
+                game.rect(0, v.y, v.w, this.height);
+                game.rect(v.wr, v.y, WIDTH - v.wr, this.height);
+                var r = game.random(0, 2);
+                var k = game.random(5, 10);
+                if (r < 1){
+                    game.fill(255, 255, 255, game.random(70,140));
+                    game.rect(0, v.y-k, v.w, this.height+2*k);
+                    game.rect(v.wr, v.y-k, WIDTH - v.wr, this.height+2*k);
+                }
+            }
+        }
+    }
+})();
---
+++ js/drop.js
@@ -0,0 +1,81 @@
+(function(){
+	var diameter      = 20 * SCALE,
+		radius        = diameter/2,
+		radiusSquare  = Math.pow(radius,2),
+		maxTailLength = 20,
+		accelCoef     = 1 * SCALE,
+		maxX          = WIDTH - radius,
+		minX          = radius,
+		y             = HEIGHT/2,
+		top           = y + radius,
+		bot           = y - radius
+    
+    Codea.Drop = function(game){
+    	this.top      = top;
+	    this.x        = WIDTH/2;
+	    this.y        = y;
+	    this.radius   = radius;
+	    this.velx     = 0;
+	    this.tail     = [ this.x ];
+	    this.orgGravx = 0;
+	    this.game     = game;
+    };
+
+    Codea.Drop.prototype = {
+		left: function(){
+    		return this.x - radius;
+    	},
+		right: function(){
+		    return this.x + radius;
+		},
+		updateTail: function(){
+		    var tail = this.tail;
+		    tail.unshift(this.x);
+		    if (tail.length == maxTailLength) tail.pop();
+		},
+
+		updatePosition: function(){
+		    this.velx = this.velx + (this.game.gravity.x - this.orgGravx) * accelCoef;
+		    this.x   = this.x + this.velx;
+		    
+		    if (this.x > maxX) {
+		        this.velx = 0;
+		        this.x = maxX;
+		    } else if (this.x < minX) {
+		        this.velx = 0;
+		        this.x = minX;
+		    }
+		},
+
+		collideBearable: function(btop, bbot){
+		    var hSquare = Math.pow(Math.min( Math.abs(bbot - y), Math.abs(btop - y) ), 2);
+		    if (radiusSquare > hSquare) return Math.sqrt(radiusSquare - hSquare);
+		    else return radius;
+		},
+
+		collide: function(block){
+		    var btop = block.y + block.height, bbot = block.y;
+		    if (btop >= bot && bbot <= top) {
+		        var bearable = this.collideBearable(btop, bbot);
+		        if (this.x - block.w <= bearable || block.wr - this.x <= bearable)
+		            return true;
+		    }
+		    return false;
+		},
+
+		draw: function(){
+			
+			var game = this.game;
+		    game.fill(255, 255, 255, 1);
+		    var tail = this.tail;
+		    for (var i = 0, l = tail.length; i < l; i++) {
+		        game.circle(tail[i], y + i * game.speed, (diameter - diameter * i/tail.length));
+		    }
+		    
+		    game.circle(this.x, y, diameter);
+		    game.fill(0, 0, 0, 1);
+		    game.circle(this.x - diameter/4 - 1, y-1, diameter/4);
+		    game.circle(this.x + diameter/4 + 1, y-1, diameter/4);
+		}
+    }
+})();
