003f3dcb6e607da477c375fd5311b9dc54ac5229
dotabox <box.dota@gmail.com>
2014-03-17 00:31:20 +0700

    comment j day

--- www/js/fbconnect.js
+++ www/js/fbconnect.js
@@ -1,4 +1,4 @@
-alert("load  FBConnect");
+alert("load FBConnect");
 (function(){
     var BKGM = BKGM||{}; 
     function dataURItoBlob(dataURI) {
@@ -19,7 +19,7 @@ alert("load  FBConnect");
             app_id=obj.appId;
         }
         var loaded=0;
-        var _onLoad=function(){
+        function _onLoad(){
             loaded++;alert("load dc");
             if(loaded==2){
 
@@ -30,9 +30,9 @@ alert("load  FBConnect");
                     alert(e);
                 }
                 if (callback) callback();
-                }       
+            }       
                 
-        }
+        };
         if (BKGM.loadJS)  {
             alert("load loadJS");
             if (cordova){
@@ -44,98 +44,9 @@ alert("load  FBConnect");
             }
 
            
-        }
+        };
         return this;
     }
-    BKGM.FBConnect.prototype= {
-        logout:function(callback) {
-            var self=this;
-            FB.logout(function(response) {
-                if(callback) callback(response);
-            });
-        },            
-        login:function(callback) {
-            var self=this;
-            FB.login(
-                function(response) {
-                    if (response.session) {
-                        if(callback) callback(response);
-                    } else {
-                        if(callback) callback(response);
-                    }
-                },
-                { scope: "publish_actions" }
-            );
-        },
-        getLoginStatus: function(callback) {
-            var self=this;
-            FB.getLoginStatus(function(response) {
-                              if (response.status == 'connected') {
-                                self.isLogin=true;
-                                if (callback) callback(response);
-                              } else {
-                                self.isLogin=false;
-                                if (callback) callback(false);
-                              }
-                              });
-            return this;
-        },
-        getAuthResponse: function(callback){
-            var self=this;
-            var authResponse = {};
-            this.getLoginStatus(function(response){
-                if(response && response.authResponse) {authResponse=response.authResponse; if (callback) callback(authResponse);}
-                else self.login(function(response){
-                    if(response && response.authResponse) {authResponse=response.authResponse; if (callback) callback(authResponse);}
-                })
-            })
-            return authResponse;
-        },
-        postCanvas:function(message, callback) {
-            var authResponse = this.getAuthResponse();
-            if (!this.isLogin) {
-                alert('Error! Not login FB');
-                return;
-            }
-            var uid = authResponse.userID;
-            var access_token = authResponse.accessToken;
-            var canvas = document.getElementById("game");
-            var imageData = canvas.toDataURL("image/png");
-            var mess =message || "http://fb.com/BKGameMaker.com"
-            try {
-                blob = dataURItoBlob(imageData);
-            } catch (e) {
-                console.log(e);
-            }
-
-            var fd = new FormData();
-            fd.append("access_token", access_token);
-            fd.append("source", blob);
-            fd.append("message", mess);
-            try {
-                BKGM.ajax({
-                    url: "https://graph.facebook.com/me/photos?access_token=" + access_token,
-                    type: "POST",
-                    data: fd,
-                    processData: false,
-                    contentType: false,
-                    cache: false,
-                    success: function (data) {
-                        console.log("success " + data);
-                        $("#poster").html("Posted Canvas Successfully");
-                    },
-                    error: function (shr, status, data) {
-                        console.log("error " + data + " Status " + shr.status);
-                    },
-                    complete: function () {
-                        console.log("Posted to facebook");
-                    }
-                });
-
-            } catch (e) {
-                console.log(e);
-            }
-        }
-    };
+    
    
 })();
