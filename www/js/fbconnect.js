e393e9b330d234205bd487cf2ce33d108064f607
dotabox <box.dota@gmail.com>
2014-03-17 01:57:55 +0700

    comment j day

--- www/js/fbconnect.js
+++ www/js/fbconnect.js
@@ -19,7 +19,37 @@
             var self=this;
             ((typeof cordova == 'undefined') && (typeof Cordova == 'undefined')) ? this.cordova=null : this.cordova=cordova;
             alert("goi FBConnect");
-            
+            var app_id="296632137153437";
+            if (obj){
+                app_id=obj.appId;
+            }
+            var loaded=0;
+            var _onLoad=function(){
+                loaded++;alert("load dc");
+                if(loaded==2){
+
+                    try {
+                        if (self.cordova) FB.init({ appId: app_id, nativeInterface: CDV.FB, useCachedDialogs: false });
+                        else FB.init({ appId: app_id,status: true,xfbml: truecookie: true,frictionlessRequests: true,oauth: true});
+                    } catch (e) {
+                        alert(e);
+                    }
+                    if (callback) callback();
+                    }       
+                    
+            }
+            if (BKGM.loadJS)  {
+                alert("load loadJS");
+                if (self.cordova){
+                    BKGM.loadJS('cdv-plugin-fb-connect.js',_onLoad);
+                    BKGM.loadJS('facebook-js-sdk.js',_onLoad);
+                } else {
+                    _onLoad();
+                    BKGM.loadJS('//connect.facebook.net/en_US/all.js',_onLoad);
+                }
+
+               
+            }
         },
         logout:function(callback) {
             var self=this;
