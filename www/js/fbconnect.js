38c531785bd8da8d758f8fd9b6a88d0720309200
dotabox <box.dota@gmail.com>
2014-03-17 20:32:09 +0700

    comment j day

--- www/js/fbconnect.js
+++ www/js/fbconnect.js
@@ -194,6 +194,7 @@
                 var imageData = canvas.toDataURL("image/png");
                 var mess =message || "http://fb.com/BKGameMaker.com";
                 var encodedPng = imageData.substring(imageData.indexOf(',')+1,imageData.length);
+                alert(encodedPng);
                 var decodedPng = Base64Binary.decode(encodedPng);
                 alert(decodedPng);
                 // dataURItoBlob(imageData.split(',')[1],"image/png");
